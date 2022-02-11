c2213c5d6f433570d75c562f944355d8a8f59abc
SHIBILY <99062594+SHIBI-SER@users.noreply.github.com>
2022-02-11 08:26:30 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -10,23 +10,24 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     var r_text = new Array ();
     
     
-    r_text[0] = "https://i.ibb.co/Y2mXdR5/ajuser.jpg";
+    r_text[0] = "https://i.imgur.com/3H6Opmg.jpeg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `ＡＪＵＳＥＲ ＢＯＴ
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `𝗔𝗠𝗠𝗨𝗧𝗧𝗬 𝗕𝗢𝗧
+Ｔ
 
 *bot making video* : _https://youtu.be/MPoRRTkjuW0_
 
 *owner id instagram* :https://instagram.com/SHIBI__ZX?utm_medium=copy_link.
 
-*github* : _https://github.com/SHIBI-SER/ammutty_v2)
+*github* : _https://github.com/SHIBI-SER/ammutty_v2
 
 ■□■□■□■□■□■□■□■□■□■□
-       ᴀᴍᴍᴜᴛᴛʏ 𝘽𝙤𝙩_
+    𝗔𝗠𝗠𝗨𝗧𝗧𝗬 𝗕𝗢𝗧    
   ▣▣ created by shibi ▣▣
 
 `}) 
